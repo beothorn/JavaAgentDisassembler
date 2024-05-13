f850cd4b26a00489501bc97629654735b09b5b8f
beothorn <lucass@gmail.com>
2024-05-13 23:53:36 +0200

    Working constant pool read

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+# javap -v -c "jar:file:./SimpleJavaApp-1.0-SNAPSHOT.jar\!/com/example/Main.class"
 java -javaagent:./build/libs/JavaAgentExperiment-1.0-SNAPSHOT.jar -jar SimpleJavaApp-1.0-SNAPSHOT.jar
